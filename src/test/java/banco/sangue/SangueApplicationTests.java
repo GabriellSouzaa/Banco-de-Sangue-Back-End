15f7f2c8235d5958a5f8856801af312c9f812188
Gabriel Souza <2211600378@fema.edu.br>
2024-08-05 13:16:54 -0300

    feat: adicionar projeto inicial

---
+++ src/test/java/banco/sangue/SangueApplicationTests.java
@@ -0,0 +1,13 @@
+package banco.sangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
