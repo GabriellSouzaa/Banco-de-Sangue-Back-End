099e830adde980d60f792dc321d2862919bfae78
Matheus Saleh <2211600056@fema.edu.br>
2025-01-10 12:55:13 -0300

    :sparkles: feat: ajustar configurações de cors para compatibilidade com o react native

--- src/main/java/blood/bank/infra/config/cors/CorsConfig.java
+++ src/main/java/blood/bank/infra/config/cors/CorsConfig.java
@@ -10,16 +10,22 @@ import org.springframework.web.filter.CorsFilter;
 public class CorsConfig {
 
     @Bean
-    public CorsFilter corsFilter(){
+    public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
+
         config.addAllowedOrigin("http://localhost:4200");
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:4200");
+
+        config.addAllowedOrigin("http://localhost:8081");
+
+        config.addAllowedOrigin("http://172.100.127.106:8081");
+
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+
         source.registerCorsConfiguration("/**", config);
+
         return new CorsFilter(source);
     }
 }
